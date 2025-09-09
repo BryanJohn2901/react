e8d35a06f8f398be1c19891f3e344680d1d7cdbb
Bryan Johnson <63813393+BryanJohn2901@users.noreply.github.com>
2025-09-08 22:07:18 -0300

    Indrução aos comandos do projeto de pomodoro

---
+++ pomodoro/src/App.tsx
@@ -0,0 +1,14 @@
+import Heading from './components/Heading'
+
+import './styles/theme.css'
+import './styles/global.css'
+
+const App = () => {
+  return (
+    <>
+        <Heading />
+    </>
+  )
+}
+
+export default App
