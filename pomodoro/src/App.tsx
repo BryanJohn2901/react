ce907ca5063eab66bb1bbf677643ea631c95af6b
Bryan Johnson <brajohn2901@gmail.com>
2025-09-18 12:22:18 -0300

    Criação de componentes

--- pomodoro/src/App.tsx
+++ pomodoro/src/App.tsx
@@ -1,11 +1,51 @@
-import './styles/theme.css'
-import './styles/global.css'
+// App.tsx
+import Container from './components/Container'
+import CountDown from './components/CountDown'
+import DefaultInput from './components/DefaultInput'
+import Logo from './components/Logo'
+import Menu from './components/Menu'
 
-const App = () => {
+import './styles/global.css'
+import './styles/theme.css'
+
+export default function App() {
   return (
     <>
+    <Container>
+      <Logo />
+    </Container>
+
+    <Container>
+      <Menu />
+    </Container>
+
+    <Container>
+      <CountDown />
+    </Container>
+
+    <Container>
+      <form className='form' action="">
+        <div className='formRow'>
+          <DefaultInput id='input' type='text'/>
+        </div>
+
+        <div className='formRow'>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+
+        <div className='formRow'>
+          <p>Ciclos</p>
+          <p>0 0 0 0 0 0</p>
+        </div>
+
+        <div className='formRow'>
+          <button>Enviar</button>
+        </div>
+
+
+      </form>
+    </Container>
     </>
+
   )
 }
-
-export default App
---
+++ pomodoro/src/components/CountDown/index.tsx
@@ -0,0 +1,12 @@
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon} from 'lucide-react'
+import styles from './styles.module.css'
+
+const CountDown = () => {
+  return (
+    <div className={styles.container}>
+      00:00
+    </div>
+  )
+}
+
+export default CountDown
